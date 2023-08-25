8bd6196193b3b8383f2ca4559675d06d9eb69cb9
Ahmed Raza <ragix.aemi@gmail.com>
2023-08-25 14:08:01 -0700

    third commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Header from "./nav"
 import LeftContainer from "./left-container"
-// import RightContainer from "./right-container";
+import RightContainer from "./right-container";
 
 function Post(props) {
     return (
@@ -69,8 +69,8 @@ function Post(props) {
 ReactDOM.render(<div className="Parent">
     <Header />
     <LeftContainer />
+    <RightContainer />
     <Post myimgUrl="https://avatars.githubusercontent.com/u/99830227?v=4" name="Ghulam Mustafa" text='"Audi E Tron GT"' imgUrl="https://www.ccarprice.com/products/Audi-E-Tron-GT-2021_1.jpg" />
     <Post myimgUrl="https://avatars.githubusercontent.com/u/99830227?v=4" name="Ahmed Raza" text='"70 dodge-charger-matte-black-edition"' imgUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCQvTBQy_yFSbCbaf2CyPVxAmabJ6Cvtexs7Mc0vwIrBOY_njlNDykjWZovCthviCiiOY&usqp=CAU" />
     <Post myimgUrl="https://avatars.githubusercontent.com/u/99830227?v=4" name="Shaheer Raza" text='"F-150 RAPTOR"' imgUrl="https://build.ford.com/dig/Ford/F-150%20F-150/2023/HD-TILE/Image%5B%7CFord%7CF-150%20F-150%7C2023%7C1%7C1.%7C801A.W1R.145.PUM.LSC.88R.89B.A5GAA.TGA.SS5.573.924.435.57Q.168.RCA.BLDAC.55D.SPP.91X.61P.53B.55B.CCAB.CLFGP.CLMHN.64B.T7R.59R.AWD.998.67T.413.655.66C.50N.91B.76R.F150.471.SRS.588.50M.524.62W.763.59S.85P.77R.44G.XL4.LEA.%5D/EXT/1/vehicle.png" />
-    {/* <RightContainer /> */}
 </div>, document.querySelector("#root"));
